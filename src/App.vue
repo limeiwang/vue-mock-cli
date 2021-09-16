2e323d861335b94e755b8c66bddc177d806373c1
limeiwang <limw@limeiwangdeMacBook-Air.local>
2021-09-16 09:40:57 +0800

    feat: add plugin vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-
-    <el-form
-      :model="form"
-      :rules="rules"
-      ref="form"
-      label-width="100px"
-      class="demo-form"
-    >
-      <el-form-item label="活动名称" prop="name">
-        <el-input v-model="form.name"></el-input>
-      </el-form-item>
-      <el-button @click="submit">提交</el-button>
-    </el-form>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-export default {
-  name: '',
-  data() {
-    return {
-      form: {
-        name: null,
-      },
-      rules: {
-        name: [{ required: true, message: "请输入活动名称", trigger: "blur" }],
-      },
-    }
-  },
-  methods: {
-    submit() {
-      console.log('submit');
-      this.fetchData();
-    },
-    fetchData() {
-      this.$http.get('/api/test/test1').then(res => {
-        console.log(res);
-      });
-    }
-  },
-  created() {
-    this.fetchData();
-  }
-}
-</script>
 
 <style>
 #app {
@@ -54,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
