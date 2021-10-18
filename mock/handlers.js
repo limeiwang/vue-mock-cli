2adad1b732f6e801b47c09c1002651324b9017cd
limeiwang <17803125899@163.com>
2021-10-18 10:13:14 +0800

    修改JSON Server 路径

--- mock/handlers.js
+++ mock/handlers.js
@@ -1,59 +1,66 @@
 const path = require('path');
 const fs = require('fs');
 const _ = require('lodash');
-
+const os = require('os');
 // 返回路径目录数组
-function _findSync (startAbsolutePath) {
-  let result = [];
+function _findSync(startAbsolutePath) {
+    let result = [];
 
-  function finder (_p) {
-    const files = fs.readdirSync(_p);
-    files.forEach(val => {
-      const fPath = path.join(_p, val);
-      const stats = fs.statSync(fPath);
-      if (stats.isDirectory()) finder(fPath);
-      else if (stats.isFile()) result.push(fPath);
-    });
-  }
+    function finder(_p) {
+        const files = fs.readdirSync(_p);
+        files.forEach(val => {
+            const fPath = path.join(_p, val);
+            const stats = fs.statSync(fPath);
+            if (stats.isDirectory()) finder(fPath);
+            else if (stats.isFile()) result.push(fPath);
+        });
+    }
 
-  finder(startAbsolutePath);
-  return result;
+    finder(startAbsolutePath);
+    return result;
 }
 
 /**
  * 将路径转换为对象
  * ep: 'user/login.json' => { user: { login: data } }
  */
-function _handlePathToObj (_p) {
-  const arr = _p.replace('.json', '').split('/');
-  return {[arr.join('_')]: require(path.join(__dirname, 'api', _p))};
+function _handlePathToObj(_p) {
+    let sysType = os.type();
+    let arr = '';
+    if (sysType === 'Windows_NT') {
+        arr = _p.replace('.json', '').split('\\');
+    } else {
+        arr = _p.replace('.json', '').split('/');
+    }
+    return { [arr.join('_')]: require(path.join(__dirname, 'api', _p)) };
 }
+
 
 // 将data目录下的文件整合为一个db数据
-function dbHandler (startPath) {
-  let db = {};
+function dbHandler(startPath) {
+    let db = {};
 
-  const startAbsolutePath = path.join(__dirname, startPath);
-  const filePaths = _findSync(startAbsolutePath);
+    const startAbsolutePath = path.join(__dirname, startPath);
+    const filePaths = _findSync(startAbsolutePath);
 
-  filePaths.forEach(_p => {
-    // 获取相对路径
-    const relativePath = path.relative(path.join(__dirname, 'api'), _p);
-    db = _.merge(db, _handlePathToObj(relativePath));
-  });
-  return db;
+    filePaths.forEach(_p => {
+        // 获取相对路径
+        const relativePath = path.relative(path.join(__dirname, 'api'), _p);
+        db = _.merge(db, _handlePathToObj(relativePath));
+    });
+    return db;
 }
 
-function rewriterHandler (db) {
-  const rewriter = {'/api/*': '/$1'};
-  for (const key of Object.keys(db)) {
-    const tmpKey = `/${key.replace(/_/g, '/')}`;
-    rewriter[tmpKey] = `/${key}`;
-  }
-  return rewriter;
+function rewriterHandler(db) {
+    const rewriter = { '/api/*': '/$1' };
+    for (const key of Object.keys(db)) {
+        const tmpKey = `/${key.replace(/_/g, '/')}`;
+        rewriter[tmpKey] = `/${key}`;
+    }
+    return rewriter;
 }
 
 module.exports = {
-  dbHandler,
-  rewriterHandler
+    dbHandler,
+    rewriterHandler
 };
--- mock/server.js
+++ mock/server.js
@@ -1,13 +1,24 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
-const {dbHandler, rewriterHandler} = require('./handlers');
+const { dbHandler, rewriterHandler } = require('./handlers');
 // 获取db数据
 const db = dbHandler('./api');
+server.use((req, res, next) => {
+    if (req.url.includes('?')) {
+        req.url = req.url.split('?')[0];
+    }
+    if (req.method === 'POST') {
+        req.method = 'GET';
+    }
+    next();
+});
 server.use(middlewares);
 server.use(jsonServer.rewriter(rewriterHandler(db)));
 // Use router
 server.use(jsonServer.router(db));
-server.listen(9090, () => {
-  console.log('JSON Server is running');
+server.use(jsonServer.bodyParser);
+
+server.listen(9000, () => {
+    console.log('JSON Server is running');
 });
